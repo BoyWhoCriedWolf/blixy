e0eef2c2a864d18ad4ee8be5a173f1527d83f7d9
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-04-25 22:24:23 +0800

    fix: api connect

--- src/utils/api-utils.ts
+++ src/utils/api-utils.ts
@@ -17,24 +17,24 @@ axios.interceptors.response.use(
   function (response: any) {
     return response.data ? response.data : response;
   },
-  function (error: any) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    let message;
-    switch (error.status) {
-      case 500:
-        message = "Internal Server Error";
-        break;
-      case 401:
-        message = "Invalid credentials";
-        break;
-      case 404:
-        message = "Sorry! the data you are looking for could not be found";
-        break;
-      default:
-        message = error.message || error;
-    }
-    return Promise.reject(message);
-  }
+  // function (error: any) {
+  //   // Any status codes that falls outside the range of 2xx cause this function to trigger
+  //   let message;
+  //   switch (error.status) {
+  //     case 500:
+  //       message = "Internal Server Error";
+  //       break;
+  //     case 401:
+  //       message = "Invalid credentials";
+  //       break;
+  //     case 404:
+  //       message = "Sorry! the data you are looking for could not be found";
+  //       break;
+  //     default:
+  //       message = error.message || error;
+  //   }
+  //   return Promise.reject(message);
+  // }
 );
 /**
  * Sets the default authorization
