3052a762b9ff98b3eed94cfc98fab3642bc5d5a8
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-04-30 23:02:35 +0800

    fix: slaes invoice form detail

--- src/sections/documents/document-detail/DocumentDetailSaleInvoice.tsx
+++ src/sections/documents/document-detail/DocumentDetailSaleInvoice.tsx
@@ -1,25 +1,27 @@
-import { CloseOutlined, Functions, Search } from "@mui/icons-material";
+import { Add, CloseOutlined, Functions } from "@mui/icons-material";
 import {
   Box,
+  Button,
   Checkbox,
   Fab,
   FormControl,
-  Grid,
-  Icon,
   MenuItem,
   Paper,
   Select,
-  Stack,
   TextField,
   Typography,
 } from "@mui/material";
+import EditForm from "components/edit-form";
 import SearchBar from "components/search-bar";
 import PrimaryTable from "components/table";
+import { FieldType } from "types/ui-types";
+import DocumentDetailAddressForm from "./DocumentDetailAddressForm";
+import DocumentDetailPaymentForm from "./DocumentDetailPaymentForm";
 
 const DocumentDetailSalesInvoice = () => {
   return (
     <Box>
-      <Box
+      <Paper
         sx={{
           p: 2,
           mb: 1,
@@ -30,186 +32,82 @@ const DocumentDetailSalesInvoice = () => {
           Invoice to
         </Typography>
         <Typography fontWeight={500}>Organizer: Tax</Typography>
-      </Box>
+      </Paper>
       <Paper sx={{ p: 2, mb: 1 }}>
         <Typography fontWeight={600} mb={1}>
           ADDRESS
         </Typography>
-        <Grid container spacing={1}>
-          <Grid item lg={6} md={6} sm={6} xs={6}>
-            <Grid container mb={1}>
-              <Grid item lg={4} md={4} sm={4} xs={4}>
-                <Typography mt={1}>Customer</Typography>
-              </Grid>
-              <Grid item lg={8} md={8} sm={8} xs={8}>
-                <SearchBar iconPosition="right" />
-              </Grid>
-            </Grid>
-            <Grid container mb={1}>
-              <Grid item lg={4} md={4} sm={4} xs={4}>
-                <Typography mt={1}>Relationship code / KVK</Typography>
-              </Grid>
-              <Grid item lg={8} md={8} sm={8} xs={8}>
-                <TextField variant="outlined" size="small" fullWidth />
-              </Grid>
-            </Grid>
-            <Grid container mb={1}>
-              <Grid item lg={4} md={4} sm={4} xs={4}>
-                <Typography mt={1}>VAT-number</Typography>
-              </Grid>
-              <Grid item lg={8} md={8} sm={8} xs={8}>
-                <TextField variant="outlined" size="small" fullWidth />
-              </Grid>
-            </Grid>
-            <Grid container mb={1}>
-              <Grid item lg={4} md={4} sm={4} xs={4}>
-                <Typography mt={1}>Website</Typography>
-              </Grid>
-              <Grid item lg={8} md={8} sm={8} xs={8}>
-                <TextField variant="outlined" size="small" fullWidth />
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item lg={6} md={6} sm={6} xs={6}>
-            <Grid container mb={1}>
-              <Grid item lg={4} md={4} sm={4} xs={4}>
-                <Typography mt={1}>Address</Typography>
-              </Grid>
-              <Grid item lg={8} md={8} sm={8} xs={8}>
-                <TextField variant="outlined" size="small" fullWidth />
-              </Grid>
-            </Grid>
-            <Grid container mb={3}>
-              <Grid item lg={4} md={4} sm={4} xs={4}></Grid>
-              <Grid item lg={8} md={8} sm={8} xs={8}>
-                <TextField variant="outlined" size="small" fullWidth />
-              </Grid>
-            </Grid>
-            <Grid container mb={1}>
-              <Grid item lg={4} md={4} sm={4} xs={4}>
-                <Typography mt={1}>Zipcode city</Typography>
-              </Grid>
-              <Grid item lg={8} md={8} sm={8} xs={8}>
-                <TextField variant="outlined" size="small" sx={{ width: 80 }} />
-                <TextField variant="outlined" size="small" />
-              </Grid>
-            </Grid>
-            <Grid container mb={1}>
-              <Grid item lg={4} md={4} sm={4} xs={4}>
-                <Typography mt={1}>Country</Typography>
-              </Grid>
-              <Grid item lg={8} md={8} sm={8} xs={8}>
-                <FormControl sx={{ width: "100%" }}>
-                  <Select
-                    displayEmpty
-                    value=""
-                    inputProps={{ "aria-label": "Without label" }}
-                    size="small"
-                  >
-                    <MenuItem disabled value=""></MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
+        <DocumentDetailAddressForm />
       </Paper>
+
+      {/* Invoice */}
       <Paper sx={{ p: 2, mb: 1 }}>
         <Typography fontWeight={600} mb={1}>
           INVOICE
         </Typography>
-        <Grid container spacing={1} mb={1}>
-          <Grid item lg={6} md={6} sm={6} xs={6}>
-            <Grid container>
-              <Grid item lg={4} md={4} sm={4} xs={4}>
-                <Typography mt={1}>invoice number</Typography>
-              </Grid>
-              <Grid item lg={8} md={8} sm={8} xs={8}>
-                <TextField variant="outlined" size="small" fullWidth />
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item lg={6} md={6} sm={6} xs={6}>
-            <Grid container>
-              <Grid item lg={3} md={3} sm={3} xs={3}>
-                <Typography mt={1}>Amount</Typography>
-              </Grid>
-              <Grid item lg={9} md={9} sm={9} xs={9} sx={{ display: "flex" }}>
-                <FormControl sx={{ width: "40%" }}>
-                  <Select
-                    displayEmpty
-                    value=""
-                    inputProps={{ "aria-label": "Without label" }}
-                    size="small"
-                  >
-                    <MenuItem disabled value="">
-                      EUR
-                    </MenuItem>
-                  </Select>
-                </FormControl>
-                <TextField
-                  InputProps={{
-                    endAdornment: <Functions fontSize="small" />,
-                  }}
-                  size="small"
-                  fullWidth
-                />
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid container spacing={1} mb={1}>
-          <Grid item lg={6} md={6} sm={6} xs={6}>
-            <Grid container>
-              <Grid item lg={3} md={3} sm={3} xs={3}>
-                <Typography mt={1}>invoice date</Typography>
-              </Grid>
-              <Grid item lg={9} md={9} sm={9} xs={9}>
-                {/* <TextField variant="outlined" size="small" fullWidth /> */}
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item lg={6} md={6} sm={6} xs={6}>
-            <Grid container>
-              <Grid item lg={3} md={3} sm={3} xs={3}>
-                <Typography mt={1}>VAT value</Typography>
-              </Grid>
-              <Grid item lg={9} md={9} sm={9} xs={9} sx={{ display: "flex" }}>
-                <TextField
-                  InputProps={{
-                    endAdornment: <Functions fontSize="small" />,
-                  }}
-                  size="small"
-                  fullWidth
-                />
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid container>
-          <Grid item lg={3} md={3} sm={3} xs={3}>
-            <Typography mt={1}>General ledger account</Typography>
-            <Typography mt={3}>Subject</Typography>
-          </Grid>
-          <Grid item lg={9} md={9} sm={9} xs={9}>
-            <TextField
-              InputProps={{ endAdornment: <Search /> }}
-              size="small"
-              fullWidth
-              sx={{ mb: 1 }}
-            />
-            <TextField variant="outlined" size="small" fullWidth />
-          </Grid>
-        </Grid>
+
+        <EditForm
+          lg={6}
+          md={6}
+          sm={12}
+          xs={12}
+          fields={[
+            // Invoice Number
+            {
+              displayName: "Invoice Number",
+              name: "invoice_number",
+              type: FieldType.Text,
+            },
+            // Amount
+            {
+              displayName: "Amount",
+              name: "amount",
+              type: FieldType.Money,
+            },
+            // Invoice date
+            {
+              displayName: "Invoice date",
+              name: "invoice_date",
+              type: FieldType.DateTime,
+            },
+            // VAT value
+            {
+              displayName: "VAT value",
+              name: "vat_value",
+              type: FieldType.Money,
+            },
+            // General ledger account
+            {
+              displayName: "General ledger account",
+              name: "general_ledger_account",
+              type: FieldType.Choice,
+              options: [
+                "Account 1",
+                "Account 2",
+                "Account 3",
+                "Account 4",
+                "Account 5",
+              ],
+            },
+            // Subject
+            {
+              displayName: "Subject",
+              name: "subject",
+              type: FieldType.Text,
+            },
+          ]}
+        />
       </Paper>
+
+      {/* Billing Rules */}
       <Paper sx={{ p: 2, mb: 1 }}>
         <Typography fontWeight={600} mb={1}>
           BILLING RULES
         </Typography>
-        <Stack direction={"row"} spacing={2}>
-          <Icon color="success">add_circle</Icon>
-          <Typography mb={1}>Add new invoice line</Typography>
-        </Stack>
+
+        <Button startIcon={<Add />} color="success">
+          Add new invoice line
+        </Button>
         <PrimaryTable
           columns={[
             { label: "Description", name: "Description" },
@@ -261,45 +159,13 @@ const DocumentDetailSalesInvoice = () => {
           ]}
         />
       </Paper>
+
+      {/* Payment */}
       <Paper sx={{ p: 2 }}>
         <Typography fontWeight={600} mb={1}>
           PAYMENT
         </Typography>
-        <Grid container spacing={1}>
-          <Grid item lg={6} md={6} sm={6} xs={6}>
-            <Grid container>
-              <Grid item lg={4} md={4} sm={4} xs={4}>
-                <Typography mt={1}>Payment method</Typography>
-                <Typography mt={3}>Payment reference</Typography>
-              </Grid>
-              <Grid item lg={8} md={8} sm={8} xs={8}>
-                <FormControl sx={{ width: "100%", mb: 1 }}>
-                  <Select
-                    displayEmpty
-                    value=""
-                    inputProps={{ "aria-label": "Without label" }}
-                    size="small"
-                  >
-                    <MenuItem disabled value="">
-                      Transfer
-                    </MenuItem>
-                  </Select>
-                </FormControl>
-                <TextField variant="outlined" size="small" fullWidth />
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item lg={6} md={6} sm={6} xs={6}>
-            <Grid container>
-              <Grid item lg={4} md={4} sm={4} xs={4}>
-                <Typography mt={1}>Expiration date</Typography>
-              </Grid>
-              <Grid item lg={8} md={8} sm={8} xs={8}>
-                {/* datepicker */}
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
+        <DocumentDetailPaymentForm />
       </Paper>
     </Box>
   );
