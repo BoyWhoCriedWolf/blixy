4b5f8dde9da898e9a13f2b6068a2f7f3365cb33d
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-05-02 23:46:07 +0800

    fix: page title color

--- src/components/typography/page-heading/PageHeading.tsx
+++ src/components/typography/page-heading/PageHeading.tsx
@@ -1,9 +1,14 @@
 import { Grid, Typography } from "@mui/material";
 import React, { FC, PropsWithChildren, ReactNode } from "react";
+import { ColorVariant } from "types/ui-types";
 
 const PageHeading: FC<
-  PropsWithChildren<{ title?: string; actions?: ReactNode }>
-> = ({ children = null, title = "", actions = null }) => {
+  PropsWithChildren<{
+    title?: string;
+    actions?: ReactNode;
+    color?: ColorVariant;
+  }>
+> = ({ children = null, title = "", actions = null, color = "primary" }) => {
   return (
     <Grid
       container
@@ -12,7 +17,9 @@ const PageHeading: FC<
       sx={{ mb: 2 }}
     >
       <Grid item>
-        <Typography variant="h3">{children ?? title}</Typography>
+        <Typography variant="h3" color={color}>
+          {children ?? title}
+        </Typography>
       </Grid>
       <Grid item>{actions}</Grid>
     </Grid>
