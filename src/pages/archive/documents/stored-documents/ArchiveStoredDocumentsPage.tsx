87b3c6559f6fa2ee45abe753b8fdb691f2fa9b06
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-05-04 01:37:01 +0800

    debug: file browser

--- src/pages/archive/documents/stored-documents/ArchiveStoredDocumentsPage.tsx
+++ src/pages/archive/documents/stored-documents/ArchiveStoredDocumentsPage.tsx
@@ -1,10 +1,10 @@
 import { Box } from "@mui/material";
-// import FileBrowser from "components/file-browser";
+import FileBrowser from "components/file-browser";
 
 const ArchiveStoredDocumentsPage = () => {
   return (
     <Box sx={{ p: 2 }} flexGrow={1}>
-      {/* <FileBrowser /> */}
+      <FileBrowser />
     </Box>
   );
 };
