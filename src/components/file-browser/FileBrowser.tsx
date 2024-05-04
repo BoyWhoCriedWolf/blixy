53cdb00603a5e7c9881babf2af4bb60e612d9b41
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-05-04 14:57:21 +0800

    fix: file browser

--- src/components/file-browser/FileBrowser.tsx
+++ src/components/file-browser/FileBrowser.tsx
@@ -1,33 +1,15 @@
-import { Box } from "@mui/material";
-import {
-  ChonkyIconName,
-  FileArray,
-  FullFileBrowser,
-  setChonkyDefaults,
-} from "chonky";
-import { ChonkyIconFA } from "chonky-icon-fontawesome";
-
-// Somewhere in your `index.ts`:
-// @ts-ignore
-setChonkyDefaults({ iconComponent: ChonkyIconFA });
-
 const FileBrowser = () => {
-  const files: FileArray = [
-    { id: "lht", name: "Projects", isDir: true },
-    {
-      id: "mcd",
-      name: "invoice.pdf",
-      //   thumbnailUrl: "https://chonky.io/chonky-sphere-v2.png",
-      ext: "pdf",
-      icon: ChonkyIconName.pdf,
-    },
-  ];
-  const folderChain = [{ id: "xcv", name: "Demo", isDir: true }];
   return (
-    <Box sx={{ height: "100%", all: "initial" }}>
-      {/* @ts-ignore */}
-      <FullFileBrowser files={files} folderChain={folderChain} />
-    </Box>
+    <iframe
+      title="File Browser"
+      style={{
+        height: "100%",
+        width: "100%",
+        border: "none",
+        borderCollapse: "collapse",
+      }}
+      src="https://blixy-file-manager.onrender.com/"
+    />
   );
 };
 
