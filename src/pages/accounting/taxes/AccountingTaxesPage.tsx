5850ae999d06323b3f7de4046122932f3aae4806
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-05-11 01:57:52 +0800

    fix: taxes list

--- src/pages/accounting/taxes/AccountingTaxesPage.tsx
+++ src/pages/accounting/taxes/AccountingTaxesPage.tsx
@@ -1,7 +1,12 @@
+import { Paper } from "@mui/material";
 import TaxesList from "sections/taxes/list";
 
 const AccountingTaxesPage = () => {
-  return <TaxesList />;
+  return (
+    <Paper sx={{ m: 2, p: 2, flexGrow: 1 }}>
+      <TaxesList />
+    </Paper>
+  );
 };
 
 export default AccountingTaxesPage;
