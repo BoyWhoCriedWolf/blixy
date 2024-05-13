63eaddf32c85c6f99607ca958885df67cd714417
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-05-14 00:51:42 +0800

    fix: document filtering feature

--- src/services/document.service.ts
+++ src/services/document.service.ts
@@ -10,14 +10,20 @@ class DocumentService extends APIService<Document> {
   async gets({
     deleted = false,
     doc_type,
+    approved,
+    general_ledger_account_id,
   }: {
     deleted?: boolean;
     doc_type?: DocumentType;
+    approved?: boolean;
+    general_ledger_account_id?: string;
   } = {}): Promise<APIResponseType<Document[]>> {
     try {
       const data = await apiClient.get(API_URLS.DOCUMENT_GETS, {
         ...(deleted ? { deleted } : {}),
         ...(doc_type ? { doc_type } : {}),
+        ...(approved ? { approved } : {}),
+        ...(general_ledger_account_id ? { general_ledger_account_id } : {}),
       });
       return data as APIResponseType<Document[]>;
     } catch (error) {
