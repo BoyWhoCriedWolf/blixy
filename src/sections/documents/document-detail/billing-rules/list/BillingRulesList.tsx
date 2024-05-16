040e2bd52b9f2bcef7e124eaeb5903251858c9c3
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-05-16 20:56:01 +0800

    fix: billing rules

--- src/sections/documents/document-detail/billing-rules/list/BillingRulesList.tsx
+++ src/sections/documents/document-detail/billing-rules/list/BillingRulesList.tsx
@@ -1,88 +1,242 @@
-import { GridRenderCellParams } from "@mui/x-data-grid";
-import TableManagement from "components/table-management";
-import React, { FC, PropsWithChildren } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import CancelIcon from "@mui/icons-material/Close";
+import DeleteIcon from "@mui/icons-material/DeleteOutlined";
+import EditIcon from "@mui/icons-material/Edit";
+import SaveIcon from "@mui/icons-material/Save";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import {
+  DataGrid,
+  GridActionsCellItem,
+  GridColDef,
+  GridEventListener,
+  GridRowEditStopReasons,
+  GridRowId,
+  GridRowModel,
+  GridRowModes,
+  GridRowModesModel,
+  GridRowsProp,
+  GridSlots,
+  GridToolbarContainer,
+  ValueOptions,
+} from "@mui/x-data-grid";
+import PageLoading from "components/loading/page-loading";
+import * as React from "react";
 import generalLedgerAccountService from "services/general.ledger.account.service";
-import { BillingRule } from "services/types/billing.rule.types";
-import { Document } from "services/types/document.types";
 import { GeneralLedgerAccount } from "services/types/general.ledger.account.types";
-import { FieldType } from "types/ui-types";
 import { joinStrings } from "utils/string-utils";
+import { v4 as uuidv4 } from "uuid";
 
-const BillingRulesList: FC<
-  PropsWithChildren<{ document?: Document; readOnly?: boolean }>
-> = ({ document, readOnly = false }) => {
+const initialRows: GridRowsProp = [];
+
+interface EditToolbarProps {
+  setRows: (newRows: (oldRows: GridRowsProp) => GridRowsProp) => void;
+  setRowModesModel: (
+    newModel: (oldModel: GridRowModesModel) => GridRowModesModel
+  ) => void;
+}
+
+function EditToolbar(props: EditToolbarProps) {
+  const { setRows, setRowModesModel } = props;
+
+  const handleNew = () => {
+    const id = uuidv4();
+    setRows((oldRows) => [...oldRows, { id, name: "", age: "", isNew: true }]);
+    setRowModesModel((oldModel) => ({
+      ...oldModel,
+      [id]: { mode: GridRowModes.Edit, fieldToFocus: "name" },
+    }));
+  };
+
   return (
-    <TableManagement<BillingRule>
-      readOnly={readOnly}
-      pageTitle=""
-      title="Billing Rule"
-      columns={[
-        { headerName: "Description", field: "description" },
-        {
-          headerName: "General Ledger Account",
-          field: "general_ledger_account_id",
-          renderCell: (p: GridRenderCellParams<BillingRule>) =>
-            joinStrings(
-              " ",
-              p?.row?.general_ledger_account?.code,
-              p?.row?.general_ledger_account?.description
-            ),
-        },
-        { headerName: "Amount excl. VAT", field: "amount_excl_vat" },
-      ]}
-      fields={[
-        {
-          displayName: "Description",
-          name: "description",
-          type: FieldType.Text,
-        },
-        {
-          displayName: "General Ledger Account",
-          name: "general_ledger_account_id",
-          type: FieldType.Choice,
-          getOptions: async () => {
-            const ret = await generalLedgerAccountService.gets();
-            if (ret.success) {
-              return ret.data ?? [];
-            }
-            return [];
-          },
-          getOptionLabel: (option?: GeneralLedgerAccount) =>
-            joinStrings(" ", option?.code, option?.description),
-          getOptionValue: (option?: GeneralLedgerAccount) => option?.id ?? "",
-          joinedFieldName: "general_ledger_account",
-        },
-        {
-          displayName: "Amount excl. VAT",
-          name: "amount_excl_vat",
-          secondaryName: "amount_excl_vat_currency",
-          type: FieldType.Money,
-        },
-      ]}
-      formatData={(v: Array<BillingRule>) => [
-        ...(v ?? []),
-        {
-          general_ledger_account: { description: "Total incl. VAT" },
-          amount_excl_vat: v.reduce(
-            (ret, cur) => ret + (cur.amount_excl_vat ?? 0),
-            0
-          ),
-          disableAction: true,
-        },
-        {
-          general_ledger_account: { description: "Invoice amount" },
-          amount_excl_vat: v.reduce(
-            (ret, cur) => ret + (cur.amount_excl_vat ?? 0),
-            0
-          ),
-          disableAction: true,
-        },
-      ]}
-      hideFooterPagination
-      enableMockup
-      inlineEdit
-    />
+    <GridToolbarContainer>
+      <Button color="primary" startIcon={<AddIcon />} onClick={handleNew}>
+        Add record
+      </Button>
+    </GridToolbarContainer>
   );
-};
+}
 
-export default BillingRulesList;
+export default function BillingRulesList({
+  document,
+  readOnly = false,
+}: React.PropsWithChildren<{
+  document?: {};
+  readOnly?: boolean;
+}> = {}) {
+  const [rows, setRows] = React.useState(initialRows);
+  const [rowModesModel, setRowModesModel] = React.useState<GridRowModesModel>(
+    {}
+  );
+  const [accounts, setAccounts] = React.useState<Array<GeneralLedgerAccount>>(
+    []
+  );
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const handleRowEditStop: GridEventListener<"rowEditStop"> = (
+    params,
+    event
+  ) => {
+    if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+      event.defaultMuiPrevented = true;
+    }
+  };
+
+  const handleEditClick = (id: GridRowId) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+  };
+
+  const handleSaveClick = (id: GridRowId) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+  };
+
+  const handleDeleteClick = (id: GridRowId) => () => {
+    setRows(rows.filter((row) => row.id !== id));
+  };
+
+  const handleCancelClick = (id: GridRowId) => () => {
+    setRowModesModel({
+      ...rowModesModel,
+      [id]: { mode: GridRowModes.View, ignoreModifications: true },
+    });
+
+    const editedRow = rows.find((row) => row.id === id);
+    if (editedRow!.isNew) {
+      setRows(rows.filter((row) => row.id !== id));
+    }
+  };
+
+  const processRowUpdate = (newRow: GridRowModel) => {
+    const updatedRow = { ...newRow, isNew: false };
+    setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+    return updatedRow;
+  };
+
+  const handleRowModesModelChange = (newRowModesModel: GridRowModesModel) => {
+    setRowModesModel(newRowModesModel);
+  };
+
+  const loadOptions = async () => {
+    setIsLoading(true);
+    const retAccounts = await generalLedgerAccountService.gets();
+    setIsLoading(false);
+
+    if (retAccounts.success) {
+      setAccounts(retAccounts.data ?? []);
+    }
+  };
+
+  React.useEffect(() => {
+    loadOptions();
+  }, []);
+
+  const columns: GridColDef[] = [
+    {
+      field: "description",
+      headerName: "Description",
+      flex: 1,
+      editable: true,
+    },
+    {
+      field: "general_ledger_account_id",
+      headerName: "General Ledger Account",
+      flex: 1,
+      editable: true,
+      type: "singleSelect",
+      valueOptions: accounts.map(
+        (item) =>
+          ({
+            label: joinStrings(" ", item.code, item.description),
+            value: item.id ?? "",
+          } as ValueOptions)
+      ),
+    },
+    {
+      field: "amount_excl_vat",
+      headerName: "Amount excl. VAT",
+      type: "number",
+      flex: 1,
+      align: "left",
+      headerAlign: "left",
+      editable: true,
+    },
+    {
+      field: "actions",
+      type: "actions",
+      headerName: "Actions",
+      width: 100,
+      cellClassName: "actions",
+      getActions: ({ id }) => {
+        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+        if (isInEditMode) {
+          return [
+            <GridActionsCellItem
+              icon={<SaveIcon />}
+              label="Save"
+              sx={{
+                color: "primary.main",
+              }}
+              onClick={handleSaveClick(id)}
+            />,
+            <GridActionsCellItem
+              icon={<CancelIcon />}
+              label="Cancel"
+              className="textPrimary"
+              onClick={handleCancelClick(id)}
+              color="inherit"
+            />,
+          ];
+        }
+
+        return [
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={handleEditClick(id)}
+            color="inherit"
+          />,
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+          />,
+        ];
+      },
+    },
+  ];
+
+  return (
+    <Box
+      sx={{
+        height: 500,
+        width: "100%",
+        "& .actions": {
+          color: "text.secondary",
+        },
+        "& .textPrimary": {
+          color: "text.primary",
+        },
+      }}
+    >
+      <PageLoading open={isLoading} />
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        editMode="row"
+        rowModesModel={rowModesModel}
+        onRowModesModelChange={handleRowModesModelChange}
+        onRowEditStop={handleRowEditStop}
+        processRowUpdate={processRowUpdate}
+        slots={{
+          toolbar: EditToolbar as GridSlots["toolbar"],
+        }}
+        slotProps={{
+          toolbar: { setRows, setRowModesModel },
+        }}
+      />
+    </Box>
+  );
+}
