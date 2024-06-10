347324195268f7bb4652ce54a9733a44259af09c
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-06-11 02:53:59 +0800

    fix: administrations page

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -62,7 +62,7 @@ export const router = createBrowserRouter(
 
       {/* Private Routes */}
       <Route
-        path="/administrations"
+        path="/"
         element={
           <Layout showSidebar={false}>
             <AdministrationsPage />
@@ -70,7 +70,6 @@ export const router = createBrowserRouter(
         }
       />
       <Route path="/" element={<Layout />}>
-        <Route index element={<Dashboard />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/home" element={<Dashboard />} />
 
