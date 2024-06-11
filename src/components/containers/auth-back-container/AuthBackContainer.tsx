f90333afe62a87f2766b9d5187a249cf6d0c7121
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-06-11 23:23:15 +0800

    fix: auth style

--- src/components/containers/auth-back-container/AuthBackContainer.tsx
+++ src/components/containers/auth-back-container/AuthBackContainer.tsx
@@ -35,7 +35,7 @@ const AuthBackContainer: FC<PropsWithChildren<{ title?: string }>> = ({
           }}
         >
           {title ? <Typography>{title}</Typography> : null}
-          {children}
+          <Box sx={{ maxHeight: "70vh", overflow: "auto" }}>{children}</Box>
         </Box>
       </Container>
 
