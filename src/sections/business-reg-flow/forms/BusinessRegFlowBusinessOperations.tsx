cb32e03b274375a6034b852c0c9302cdd385dc0a
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-06-12 23:23:21 +0800

    feat: business operation form

--- src/sections/business-reg-flow/forms/BusinessRegFlowBusinessOperations.tsx
+++ src/sections/business-reg-flow/forms/BusinessRegFlowBusinessOperations.tsx
@@ -1,7 +1,60 @@
-import React from "react";
+import { Grid } from "@mui/material";
+import AnimBusiness from "components/animations/AnimBusiness";
+import EditForm from "components/edit-form";
+import { FC, PropsWithChildren } from "react";
+import { Administration } from "services/types/administration.types";
+import { DispatchFunction, FieldType } from "types/ui-types";
 
-const BusinessRegFlowBusinessOperations = () => {
-  return <div>BusinessRegFlowBusinessOperations</div>;
+const BusinessRegFlowBusinessOperations: FC<
+  PropsWithChildren<{
+    data?: Administration;
+    onChange?: DispatchFunction<Administration>;
+  }>
+> = ({ data, onChange }) => {
+  return (
+    <Grid container alignItems={"center"}>
+      <Grid item lg={6} md={6} sm={6} xs={12}>
+        <AnimBusiness />
+      </Grid>
+      <Grid
+        item
+        lg={6}
+        md={6}
+        sm={6}
+        xs={12}
+        sx={{ maxHeight: "50vh", overflow: "auto" }}
+      >
+        <EditForm
+          data={data}
+          onChange={onChange}
+          fields={[
+            {
+              displayName: "Operational Start Date",
+              name: "operation_start_date",
+              type: FieldType.DateOnly,
+            },
+            {
+              displayName: "Fiscal Year End",
+              name: "operation_fiscal_end_year",
+              type: FieldType.Integer,
+            },
+
+            {
+              displayName: "Accounting Method",
+              name: "operation_accounting_method",
+              type: FieldType.Choice,
+              options: ["Cash basis", "Accrual basis"],
+            },
+            {
+              displayName: "Business Activities",
+              name: "operation_business_activities",
+              type: FieldType.MultiLineText,
+            },
+          ]}
+        />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default BusinessRegFlowBusinessOperations;
